eaf5ac6f0e0a29b7cb2a7a1552a7bc5dc3d497bf
selee <selee@student.42lyon.fr>
2021-09-20 13:03:17 +0200

    commit builtin; added builtin 1 and 2

---
+++ src/builtin_1.c
@@ -0,0 +1,14 @@
+#include "minishell.h"
+
+/*
+- [echo] with option -n
+- [cd] with only a relative or absolute path
+- [pwd] with no options
+- [export] with no options
+- [unset] with no options
+- env with no options or arguments 
+- exit with no options
+*/
+
+
+
