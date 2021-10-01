cf83838c35cba21cacbca31dbaba07e1153b8c40
selee <selee@student.42lyon.fr>
2021-10-01 13:40:23 +0200

    cd

--- src/built_in/cd.c
+++ src/built_in/cd.c
@@ -21,11 +21,11 @@ void	builtin_cd(t_vars *vars, t_token *current_token, char **envp)
 
 	current_token = current_token->next;
 	path_temp = current_token->buffer.str;
-printf("path_temp:%s\n", path_temp);
+// printf("path_temp:%s\n", path_temp);
 	if (path_temp[0] == '~')
 		path = ft_strjoin(search_home(envp), ft_substr(path_temp, 1, ft_strlen(path_temp) - 1));
 	else
 		path = path_temp;
-printf("path:%s\n", path);
+// printf("path:%s\n", path);
 	chdir(path);
 }
