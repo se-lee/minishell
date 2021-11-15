5bd8c62e63a4074a4cf0ebe47a55b35457f47658
selee <selee@student.42lyon.fr>
2021-11-15 15:20:30 +0100

    exit re-redo; took it back to the original version, now need to reduce lines

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -37,25 +37,32 @@ long  long int	ft_atolli(const char *str)
 
 void	builtin_exit(t_vars *vars, t_command *current_cmd)
 {
-	if (current_cmd->command[1])
+	if (current_cmd->command[1] == NULL)
+		exit(vars->return_value);
+	else if (current_cmd->command[1])
 	{
-		if (current_cmd->command[1][0] == ' ') //Added
-			remove_space(&current_cmd->command[1]);
-		vars->return_value = ft_atolli(current_cmd->command[1]);
-		if (strisnum(current_cmd->command[1]) == 1 && current_cmd->command[2] == NULL)
+		if (strisnum(current_cmd->command[1]) == 1
+			&& current_cmd->command[2] == NULL)
 		{
+			vars->return_value = ft_atolli(current_cmd->command[1]);		
 			if (value_exceeds_llint(current_cmd->command[1], vars->return_value) == TRUE)
 				display_error_and_exit(current_cmd, "numeric argument required", 255);
 			if (vars->return_value > 255 || vars->return_value < 0)
-				vars->return_value = vars->return_value % 256;		
+				vars->return_value = vars->return_value % 256;
+			if (count_command(vars->cmd) == 1)
+				printf("exit\n");
+			exit(vars->return_value);
 		}
 		else if (strisnum(current_cmd->command[1]) == 1
 			&& current_cmd->command[2] != NULL)
+		{
+			printf("exit\n");
 			display_cmd_error(current_cmd, "too many arguments", FALSE);
+			vars->return_value = 1;
+		}
 		else
 			display_error_and_exit(current_cmd, "numeric argument required", 255);
-	}
 	if (count_command(vars->cmd) == 1) //current_cmd->pipe == 0)
 		printf("exit\n");
-	exit(vars->return_value);
+	}
 }
