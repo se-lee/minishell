ccca2821edc65fa4a87740c8b6dab58635a02ef2
selee <selee@student.42lyon.fr>
2021-11-15 06:01:35 +0100

    added remove_space to handle exit '    123    '

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -16,6 +16,15 @@ int	strisnum(char *str)
 	return (1);
 }
 
+void	remove_space(char **str)
+{
+	char	*temp;
+
+	temp = ft_strtrim(*str, " ");
+	free(*str);
+	*str = temp;
+}
+
 void	display_error_and_exit(t_command *current_cmd, char *message, int exit_status)
 {
 	display_cmd_error(current_cmd, message, TRUE);
@@ -31,10 +40,13 @@ void	builtin_exit(t_vars *vars, t_command *current_cmd)
 	}
 	else if (current_cmd->command[1])
 	{
+		if (current_cmd->command[1][0] == ' ') //Added
+			remove_space(&current_cmd->command[1]);
 		if (strisnum(current_cmd->command[1]) == 1
 			&& current_cmd->command[2] == NULL)
 		{
-			vars->return_value = atoi(current_cmd->command[1]);
+			vars->return_value = ft_atoi(current_cmd->command[1]); //atoi -> ft_atoi
+printf("ret:%d\n", vars->return_value);
 			if (vars->return_value > 255 || vars->return_value < 0)
 				vars->return_value = vars->return_value % 256;
 			if (count_command(vars->cmd) == 1) //current_cmd->pipe == 0)
