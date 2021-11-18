791a98d97167081867024765b7768659555d198e
selee <selee@student.42lyon.fr>
2021-11-18 11:00:58 +0100

    exit fix -- argument with spaces

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -35,6 +35,7 @@ long  long int	ft_atolli(const char *str)
 	return (sign * out);
 }
 
+/*
 void	builtin_exit(t_vars *vars, t_command *current_cmd)
 {
 	if (current_cmd->command[1] == NULL)
@@ -66,3 +67,36 @@ void	builtin_exit(t_vars *vars, t_command *current_cmd)
 		printf("exit\n");
 	}
 }
+*/
+
+void	builtin_exit(t_vars *vars, t_command *current_cmd)
+{
+	if (current_cmd->command[1] == NULL)
+	{
+		printf("exit\n");
+		exit(vars->return_value);
+	}
+	else if (current_cmd->command[1])
+	{
+		if (current_cmd->command[1][0] == ' ')
+			remove_space(&current_cmd->command[1]);
+		if (strisnum(current_cmd->command[1]) == 1
+			&& current_cmd->command[2] == NULL)
+		{
+			vars->return_value = ft_atolli(current_cmd->command[1]);
+			if (value_exceeds_llint(current_cmd->command[1], vars->return_value) == TRUE)
+				display_error_and_exit(current_cmd, "numeric argument required", 255);
+			if (vars->return_value > 255 || vars->return_value < 0)
+				vars->return_value = vars->return_value % 256;
+			if (count_command(vars->cmd) == 1)
+				printf("exit\n");
+			exit(vars->return_value);
+		}
+		else if (strisnum(current_cmd->command[1]) == 1
+			&& current_cmd->command[2] != NULL)
+			display_cmd_error(current_cmd, "too many arguments", FALSE);
+		else
+			display_error_and_exit(current_cmd, "numeric argument required", 255);
+	}
+}
+
